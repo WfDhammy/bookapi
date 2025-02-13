d985339652bed9ca63af516614a2478dd79c3e49
WfDhammy <abimbolaokeyedun919@gmail.com>
2025-02-13 08:25:45 -0600

    firstcommit

---
+++ server/api/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import main, authors, get_author, create_author, delete_author, update_author, courses, create_course, update_course, get_course, delete_course
+
+
+
+urlpatterns = [
+    path('', main, name="autho"),
+    path('author/', authors, name="All authors"),
+    path('author/<uuid:id>', get_author, name="authors"),
+    path('author/create', create_author, name="create author"),
+    path('author/delete/<uuid:id>', delete_author, name="delete author"),
+    path('author/update/<uuid:id>', update_author, name="update author"),
+    path('course/', courses, name="All courses"),
+    path('course/<uuid:id>', get_course, name="course"),
+    path('course/create', create_course, name="create course"),
+    path('course/delete/<uuid:id>', delete_course, name="delete course"),
+    path('course/update/<uuid:id>', update_course, name="update course"),
+]
